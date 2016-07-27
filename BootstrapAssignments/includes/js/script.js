0bcc34f9022f9f1e468de0abed3f837107fdcc65
spsingh559 <shyam.singh4@wipro.com>
2016-07-27 04:21:25 +0000

    Bootstrap Assignment

---
+++ BootstrapAssignments/includes/js/script.js
@@ -0,0 +1,48 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(function () {
+
+	/*
+	$('#alertMe').click(function(e)
+	{
+		e.preventDefault();
+		$('#successAlert').slideDown();
+	});
+	*/
+	$('#formBtnSubmit').click(function(m)
+	{
+		m.preventDefault();
+		$('#successAlertForm').slideDown();
+	});
+
+	/* script for mobile call */
+
+	$('#alertMobile').click(function(m)
+	{
+		m.preventDefault();
+		$('#successAlert').slideDown();
+	});
+
+	$('#btnMessage').click(function(m)
+	{
+		m.preventDefault();
+		$('.message-wrapper').slideDown();
+
+	});
+
+	$('#btnAlertTextArea').click(function(m)
+	{
+		m.preventDefault();
+		$('#successMailAlert').slideDown();
+		$(".message-wrapper").css("display", "none");
+	});
+
+});
